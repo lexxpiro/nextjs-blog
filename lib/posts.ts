05826c177ad5bbc1047edecde2ce62bb2360c5dd
lexxpiro <lexxpiro@gmail.com>
2022-11-16 13:38:54 +0700

    ts commit

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...matterResult.data as { date: string; title: string },
     };
   });
   // Sort posts by date
@@ -63,7 +63,7 @@ export function getAllPostIds() {
   });
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
@@ -75,11 +75,11 @@ export async function getPostData(id) {
       .use(html)
       .process(matterResult.content);
   const contentHtml = processedContent.toString();
-  
+
   // Combine the data with the id and contentHtml
   return {
       id,
       contentHtml,
-      ...matterResult.data,
+      ...matterResult.data as { date: string; title: string },
   };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,9 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -12,8 +13,8 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
   return {
       props: {
       postData,
@@ -21,9 +22,17 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
     return (
-      <Layout>
+      <Layout home={undefined}>
         <Head>
           <title>{postData.title}</title>
         </Head>
